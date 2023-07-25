66e8fce0d502b53da79c8cd459178b37d40a0d12
Harish-31 <137464364+Harish-31@users.noreply.github.com>
2023-07-25 12:07:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function black(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/nb80ota_1501211.jpg?q=75")
+}
+function blue(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/anh0ota_1501209.jpg?q=75")
+}
+function red(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/7aqr3va_1618881.jpg?q=75")
+}
+function white(){
+    document.getElementById("img").setAttribute("src","https://imgd.aeplcdn.com/370x208/n/p070ota_1501215.jpg?q=75")
+}
